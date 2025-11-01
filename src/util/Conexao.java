d3d107ffed1cb5347d0f974c4864963d3e51d97d
Kissuke <pedrohomeofficework@gmail.com>
2025-11-01 15:16:03 -0300

    readme

--- src/util/Conexao.java
+++ src/util/Conexao.java
@@ -1,4 +1,8 @@
 package util;
 
+/*Essa classe serve para Abrir uma conexão com o MySQL,
+*Retornar essa conexão para qualquer outra classe usar e Avisar se deu erro.
+* */
+
 public class Conexao {
 }
