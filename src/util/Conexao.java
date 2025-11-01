7e224d707d86cd5f5d27e9f3b0988215d17ad748
Kissuke <pedrohomeofficework@gmail.com>
2025-11-01 15:38:33 -0300

    conexao - util - finalizada

--- src/util/Conexao.java
+++ src/util/Conexao.java
@@ -4,5 +4,28 @@ package util;
 *Retornar essa conexão para qualquer outra classe usar e Avisar se deu erro.
 * */
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
-}
+    private static final String url = "jdbc:mysql://localhost:3306/meubanco";
+    private static final String user = "root";
+    private static final String pass = "1234";
+
+    private static Connection conn;
+
+    public static Connection getConn() {
+        try {
+            if (conn == null) {
+                conn = DriverManager.getConnection(url, user, pass);
+                return conn;
+            } else {
+                return conn;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    }
