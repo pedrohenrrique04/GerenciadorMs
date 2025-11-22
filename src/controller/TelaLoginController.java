8822c08801bbc78f316de0072104138382b9724c
Kissuke <pedrohomeofficework@gmail.com>
2025-11-22 00:59:05 -0300

    telaLoginConcluida

--- src/controller/TelaLoginController.java
+++ src/controller/TelaLoginController.java
@@ -1,4 +1,48 @@
 package controller;
 
+import Dao.UsuarioDAO;
+import Model.Usuario;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class TelaLoginController {
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    private PasswordField txtSenha;
+
+    @FXML
+    private Button btnLogin;
+
+    private UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+    @FXML
+    public void initialize() {
+        btnLogin.setOnAction(e -> autenticarUsuario());
+    }
+
+    private void autenticarUsuario() {
+        String nome = txtUsuario.getText();
+        String senha = txtSenha.getText();
+
+        if (autenticar(nome, senha)) {
+            System.out.println("Login realizado com sucesso!");
+            // aqui você pode trocar de tela
+        } else {
+            System.out.println("Usuário ou senha inválidos!");
+        }
+    }
+
+    public boolean autenticar(String nome, String senha) {
+        return usuarioDAO.validarLogin(nome, senha);
+    }
+
+    public void cadastrarUsuario(String nome, String senha) {
+        Usuario usuario = new Usuario(nome, senha);
+        usuarioDAO.cadastrarUsuario(usuario);
+    }
 }
---
+++ src/view/TelaLogonView.java
@@ -0,0 +1,22 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TelaLogonView extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/TelaLogin.fxml"));
+        System.out.println(getClass().getResource("/view/TelaLogin.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
