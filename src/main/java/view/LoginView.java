9cc4638670bba574037365ab988fbaae345b53a5
vertoxiz <75530138+Everton123o@users.noreply.github.com>
2025-11-22 03:53:05 -0300

    maincerta

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -6,8 +6,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class LoginView extends JFrame {
 
@@ -83,7 +81,7 @@ public class LoginView extends JFrame {
             }
         });
 
-        // cria o controller
+        // cria o main.controller
         UsuarioController usuarioController = new UsuarioController();
 
         // ação do botão
--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -8,8 +8,9 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/Relatorios.fxml"));
-        System.out.println(getClass().getResource("/view/Relatorios.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Relatorios.fxml"));
+
+        System.out.println(getClass().getResource("/view/Relatorios.fxml")); // deve imprimir algo != null
 
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Relatórios");
